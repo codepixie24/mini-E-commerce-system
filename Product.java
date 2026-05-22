bbd0002cad75f1423e3493000f84c7c2189ba1dc
codepixie24 <kefilwekadynkalaloka@gmail.com>
2026-05-22 21:48:31 +0200

    Create Product class with attributes and display method
    i used Encapsulation

---
+++ Product.java
@@ -0,0 +1,15 @@
+public class Product{
+  
+  private String id;
+  private String name;
+  private double price;
+  
+  public Product( String id, String name, double price){
+    this.id = id;
+    this.name = name;
+    this. price = price;
+
+    public void displayProduct(){
+      System.out.println("ID:" + id +"Name:" + name + "Price"+ price);
+    }
+}
