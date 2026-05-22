4ca38addd232a4bc5c9a0b0796db6d6179d02999
codepixie24 <kefilwekadynkalaloka@gmail.com>
2026-05-22 23:00:03 +0200

    Refactor Cart class methods and fix initialization
    Fix issues in Cart class including method names and initialization.

--- Cart.java
+++ Cart.java
@@ -2,5 +2,32 @@ import java.util.Arraylist;
 public class Cart{
   //list to store products
   private ArrayList<Product> products ;
-  
-}
+
+  public Cart(){
+    products = new Arraylist<>();
+  }
+  public void addProduct(Product product){
+    products.add(product);
+    System.out.println(poduct.getName()+ "Added to cart")
+  }
+  public void removeProduct(Product product){
+    products.remove(product);
+    System.out.println(product.getName()+"removed from cart");
+  }
+  public void displayCart(){
+    if (products.isEmpty()){
+      System.out.println("Cart is empty!!!");
+      return; // early exitas it stops the method right here
+    }
+    System.out.println("Products in cart:");
+    for(Product product : products){
+      System.out.println(product.getName()+"-P"+ product.getPrice());
+    }
+  public double CalculateTotal(){
+    double total = 0;
+    for(Product product: products){
+      total+= product.getPrice();
+    }
+    return total;
+  }
+  }
