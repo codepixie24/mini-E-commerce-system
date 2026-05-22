e58c1606e297f2e1bbc89a04579f413e6e6f8e41
codepixie24 <kefilwekadynkalaloka@gmail.com>
2026-05-22 22:32:01 +0200

    Add stock attribute and related methods to Product
    Updated Product class to include stock management.

--- Product.java
+++ Product.java
@@ -3,12 +3,29 @@ public class Product{
   private String id;
   private String name;
   private double price;
+  private int Stock;
   
-  public Product( String id, String name, double price){
+  public Product( String id, String name, double price, int stock){
     this.id = id;
     this.name = name;
     this. price = price;
-
+    this.stock = stock;
+  }
+  public String getName(){
+    return name;
+  }
+  public string getId(){
+    return id;
+  }
+  public double getPrice(){
+    return price;
+  }
+  public int getStock(){
+    return stock;
+  }
+  public void setStock(){
+    this.stock= stock;
+  }
     public void displayProduct(){
       System.out.println("ID:" + id +"Name:" + name + "Price"+ price);
     }
